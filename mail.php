db6270fee91649ce6f143f178ff02cc15c59b3c0
jayant-verma <iamvermajaynt@gmail.com>
2020-12-09 20:01:16 +0530

    first commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+﻿
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("iamvermajayant@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
